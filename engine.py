f64908ec938cb6271c8688ab93b100ddd548609a
Bill Turner <billturner@gmail.com>
2019-06-28 20:26:26 -0400

    Step 2 complete

--- engine.py
+++ engine.py
@@ -13,14 +13,25 @@ def main():
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     colors = {
-        "dark_wall": libtcod.Color(0, 0, 100),
-        "dark_ground": libtcod.Color(50, 50, 150),
+        "dark_wall": libtcod.Color(169, 169, 169),  # CSS DarkGray
+        "dark_ground": libtcod.Color(0, 0, 0)  # CSS Black
     }
+
+    # initial entities
+    player = Entity(int(screen_width / 2),
+                    int(screen_height / 2), "@", libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5),
+                 int(screen_height / 2), "@", libtcod.yellow)
+    entities = [player, npc]
 
     libtcod.console_set_custom_font(
         "fonts/consolas12x12.png",
-        libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD,
+        libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD
     )
     libtcod.console_init_root(
         screen_width, screen_height, "Roguelike Tutorial LibTCOD", False
@@ -29,25 +40,23 @@ def main():
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size,
+                      map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
-    # initial entities
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", libtcod.white)
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", libtcod.yellow)
-    entities = [player, npc]
-
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        render_all(con, entities, game_map,
+                   screen_width, screen_height, colors)
 
         libtcod.console_flush()
 
-        action = handle_keys(key)
-
         clear_all(con, entities)
+
+        action = handle_keys(key)
 
         move = action.get("move")
         exit = action.get("exit")
