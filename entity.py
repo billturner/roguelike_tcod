060eef5fb38653c16a6d4614917033a73605c40c
Bill Turner <billturner@gmail.com>
2019-07-03 17:23:09 -0400

    Step 5 complete

--- entity.py
+++ entity.py
@@ -13,3 +13,11 @@ class Entity:
         # move entity a given amount
         self.x += dx
         self.y += dy
+
+
+def get_blocking_entities_at_location(entities, destination_x, destination_y):
+    for entity in entities:
+        if entity.blocks and entity.x == destination_x and entity.y == destination_y:
+            return entity
+
+    return None
